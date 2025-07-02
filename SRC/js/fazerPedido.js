c6886e35311747fbe0501053da2ad38ab66822ce
Alex Santos <1984alexsantos@gmail.com>
2025-07-02 16:08:18 -0300

    atualização-dois

--- SRC/js/fazerPedido.js
+++ SRC/js/fazerPedido.js
@@ -45,7 +45,7 @@ function validarBtnUm() {
         } 
     })
     
-    if (validarRadioSelecionado('tamanho', 'cor'), nameInput.value.trim() !== '') {
+    if (validarRadioSelecionado('tamanho', 'cor') && nameInput.value.trim() !== '') {
         window.open('http://127.0.0.1:5500/SRC/html/anuncios/anuncio-01/pagamento-01.html', '_blank');
     }
 }
