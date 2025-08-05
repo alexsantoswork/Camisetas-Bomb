257b3ef2beb0e622f4504f415093a6c7561ab63f
Alex Santos <1984alexsantos@gmail.com>
2025-08-04 21:29:24 -0300

    edit-btn-wp

---
+++ SRC/js/navegar-loja.js
@@ -0,0 +1,13 @@
+// Função JavaScript para alternar a classe
+        function navegacaoLojaVisivel() {
+            
+            const conteudoDiv = document.getElementById('navegarLoja');
+
+            if (conteudoDiv.classList.contains('navegar-loja-visivel')) {
+                conteudoDiv.classList.remove('navegar-loja-visivel');
+                conteudoDiv.classList.add('navegar-loja');
+            } else {
+                conteudoDiv.classList.add('navegar-loja-visivel');
+                conteudoDiv.classList.remove('navegar-loja'); // Opcional: remove a classe de ocultar
+            }
+        }
